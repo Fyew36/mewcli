6ce9b8445039a386920dd6c01aa8504caad3dcb6
sukvatXD <c.nam020213@gmail.om>
2026-06-15 11:45:46 +0700

    Fix ColorUtil duplicate methods

--- src/main/java/mew/util/ColorUtil.java
+++ src/main/java/mew/util/ColorUtil.java
@@ -62,33 +62,3 @@ public class ColorUtil {
         GlStateManager.color(c.getRed() / 255f, c.getGreen() / 255f, c.getBlue() / 255f, c.getAlpha() / 255f);
     }
 }
-
-    public static Color interpolate(float progress, Color startColor, Color endColor) {
-        progress = Math.min(Math.max(progress, 0.0f), 1.0f);
-        return new Color((int) ((float) startColor.getRed() + progress * (float) (endColor.getRed() - startColor.getRed())), (int) ((float) startColor.getGreen() + progress * (float) (endColor.getGreen() - startColor.getGreen())), (int) ((float) startColor.getBlue() + progress * (float) (endColor.getBlue() - startColor.getBlue())));
-    }
-
-    public static Color getHealthBlend(float percent) {
-        if (percent >= 0.9f) {
-            return GREEN;
-        }
-        if (percent >= 0.55f) {
-            return ColorUtil.interpolate((percent - 0.55f) / 0.35f, YELLOW, GREEN);
-        }
-        if (percent >= 0.45f) {
-            return YELLOW;
-        }
-        if (percent >= 0.1f) {
-            return ColorUtil.interpolate((percent - 0.1f) / 0.35f, RED, YELLOW);
-        }
-        return RED;
-    }
-
-    public static Color darker(Color color, float factor) {
-        return ColorUtil.scale(color, factor, color.getAlpha());
-    }
-
-    public static Color scale(Color color, float scaleFactor, int alpha) {
-        return new Color(Math.min(Math.max((int) ((float) color.getRed() * scaleFactor), 0), 255), Math.min(Math.max((int) ((float) color.getGreen() * scaleFactor), 0), 255), Math.min(Math.max((int) ((float) color.getBlue() * scaleFactor), 0), 255), alpha);
-    }
-}
